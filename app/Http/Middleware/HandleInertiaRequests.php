5d3a5a9f84fee9a531a3b8d4e9a94df4e12b9a8d
Ivan <ivanrizqy.ir@gmail.com>
2023-12-04 16:28:44 +0700

    Filter ziggy routes share data which was exposed in data-page attributes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
                 'user' => $request->user(),
             ],
             'ziggy' => fn () => [
-                ...(new Ziggy)->toArray(),
+                ...(new Ziggy)->filter('api.*', false)->toArray(),
                 'location' => $request->url(),
             ],
         ];
