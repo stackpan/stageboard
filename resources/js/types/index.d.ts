986c6da646775c7a8691831077c4ff90e2b8b9c5
Ivan <ivanrizqy.ir@gmail.com>
2023-12-26 18:56:02 +0700

    Remove unused components

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -42,21 +42,3 @@ export type PageProps<T extends Record<string, unknown> = Record<string, unknown
     user: User
   }
 }
-
-export interface Link {
-  href: string
-  rel: string
-  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
-}
-
-export interface Links {
-  links: Record<string, Link>
-}
-
-export interface ResponseBody {
-  message: string
-}
-
-export interface ResponseBodyWithData<T> extends ResponseBody {
-  data: T
-}
